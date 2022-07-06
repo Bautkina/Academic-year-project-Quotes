6b7e75df391930d66fb65c56ae8df1e2be185f3f
Bautkina <108812985+Bautkina@users.noreply.github.com>
2022-07-06 19:40:24 +0300

    Добавление кнопок меню

--- src/main/java/com/example/newappli/Control.java
+++ src/main/java/com/example/newappli/Control.java
@@ -1,14 +1,40 @@
 package com.example.newappli;
 
+
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
-public class Control{
-    @FXML
-    private Label welcomeText;
+public class Control {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField button_field_log;
+
+    @FXML
+    private Button button_go;
+
+    @FXML
+    private Button button_login;
+
+    @FXML
+    private TextField button_pass;
+
+    @FXML
+    void initialize() {
+        assert button_field_log != null : "fx:id=\"button_field_log\" was not injected: check your FXML file 'hello-view.fxml'.";
+        assert button_go != null : "fx:id=\"button_go\" was not injected: check your FXML file 'hello-view.fxml'.";
+        assert button_login != null : "fx:id=\"button_login\" was not injected: check your FXML file 'hello-view.fxml'.";
+        assert button_pass != null : "fx:id=\"button_pass\" was not injected: check your FXML file 'hello-view.fxml'.";
+
     }
 }
+
+
